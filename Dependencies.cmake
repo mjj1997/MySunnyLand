995e08dd98099fd60c39e8ae5da9edfe9b74b320
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-04-06 12:39:13 +0800

    build: Integrate dependencies by vcpkg

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -8,5 +8,12 @@ function(MySunnyLand_setup_dependencies)
   # For each dependency, see if it's
   # already been provided to us by a parent project
 
+  find_package(SDL3 CONFIG REQUIRED)
+  find_package(SDL3_image CONFIG REQUIRED)
+  find_package(SDL3_mixer CONFIG REQUIRED)
+  find_package(SDL3_ttf CONFIG REQUIRED)
+  find_package(glm CONFIG REQUIRED)
+  find_package(nlohmann_json CONFIG REQUIRED)
+  find_package(spdlog CONFIG REQUIRED)
 
 endfunction()

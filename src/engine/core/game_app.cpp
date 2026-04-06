cbb8c11bb5ba40e7901c64990f3eb56ecbe7ddcc
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-04-06 16:14:55 +0800

    feat: Complete 主循环框架

---
+++ src/engine/core/game_app.cpp
@@ -0,0 +1,92 @@
+#include "game_app.h"
+
+#include <SDL3/SDL.h>
+#include <spdlog/spdlog.h>
+
+namespace engine::core {
+
+GameApp::~GameApp()
+{
+    if (m_isRunning) {
+        spdlog::warn("GameApp 被销毁时没有显式关闭。现在关闭。 ...");
+        clean();
+    }
+}
+
+void GameApp::run()
+{
+    if (!init()) {
+        spdlog::error("GameApp 初始化失败。");
+        return;
+    }
+
+    while (m_isRunning) {
+        float deltaTime{ 0.01f }; // 每帧的时间间隔（临时设定）
+        handleEvents();
+        update(deltaTime);
+        render();
+    }
+
+    clean();
+}
+
+bool GameApp::init()
+{
+    spdlog::trace("初始化 GameApp ...");
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)) {
+        spdlog::error("SDL 初始化失败！SDL 错误：{}", SDL_GetError());
+        return false;
+    }
+
+    m_window = SDL_CreateWindow("SunnyLand", 1280, 720, SDL_WINDOW_RESIZABLE);
+    if (m_window == nullptr) {
+        spdlog::error("SDL 创建窗口失败！SDL 错误：{}", SDL_GetError());
+        return false;
+    }
+
+    m_renderer = SDL_CreateRenderer(m_window, nullptr);
+    if (m_renderer == nullptr) {
+        spdlog::error("SDL 创建渲染器失败！SDL 错误：{}", SDL_GetError());
+        return false;
+    }
+
+    m_isRunning = true;
+    return true;
+}
+
+void GameApp::handleEvents()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_EVENT_QUIT) {
+            m_isRunning = false;
+        }
+    }
+}
+
+void GameApp::update(double deltaTime)
+{
+    // 更新游戏状态, 暂时为空
+}
+
+void GameApp::render()
+{
+    // 渲染游戏状态, 暂时为空
+}
+
+void GameApp::clean()
+{
+    spdlog::trace("关闭 GameApp ...");
+    if (m_renderer != nullptr) {
+        SDL_DestroyRenderer(m_renderer);
+        m_renderer = nullptr;
+    }
+    if (m_window != nullptr) {
+        SDL_DestroyWindow(m_window);
+        m_window = nullptr;
+    }
+    SDL_Quit();
+    m_isRunning = false;
+}
+
+} // namespace engine::core
---
+++ src/engine/core/game_app.h
@@ -0,0 +1,41 @@
+#pragma once
+
+struct SDL_Window;
+struct SDL_Renderer;
+
+namespace engine::core { // 命名空间的最佳实践：与文件路径一致
+
+/**
+ * @brief 主游戏应用程序类，初始化SDL，管理游戏循环。
+ */
+class GameApp final // final 表示该类不能被继承
+{
+public:
+    GameApp() = default;
+    ~GameApp();
+
+    // 禁止拷贝和移动
+    GameApp(const GameApp&) = delete;
+    GameApp& operator=(const GameApp&) = delete;
+    GameApp(GameApp&&) = delete;
+    GameApp& operator=(GameApp&&) = delete;
+
+    /**
+     * @brief 运行游戏应用程序，其中会调用 init()，然后进入主循环，离开循环后自动调用 clean()。
+     */
+    void run();
+
+private:
+    [[nodiscard]] bool init(); // nodiscard属性 表示该函数返回值不应该被忽略
+    void handleEvents();
+    void update(double deltaTime);
+    void render();
+    void clean();
+
+private:
+    SDL_Window* m_window{ nullptr };
+    SDL_Renderer* m_renderer{ nullptr };
+    bool m_isRunning{ false };
+};
+
+} // namespace engine::core

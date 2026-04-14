8e6280525389ed422966e15dff399f199e210e11
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-04-14 16:33:42 +0800

    feat: Add 添加组件的方法
    关键技术：
    
    1. 可变参数模板
    2. &&右值引用 + std::forward = 完美转发
    
    执行流程：
    
    1. 检查组件是否已存在
    2. 创建新的 unique_ptr
    3. 获取其裸指针（用于返回和调用 init）
    4. 设置 owner
    5. 将 unique_ptr 的所有权移动到 map 中
    6. 调用新组件的 init() 方法

--- src/engine/object/game_object.h
+++ src/engine/object/game_object.h
@@ -2,10 +2,14 @@
 
 #include "../component/component_base.h"
 
+#include <spdlog/spdlog.h>
+
 #include <concepts>
 #include <memory>
 #include <typeindex> // 用于类型索引
 #include <unordered_map>
+#include <utility> // 用于完美转发
+
 namespace engine::object {
 
 template<typename T>
@@ -28,6 +32,40 @@ public:
     GameObject& operator=(const GameObject&) = delete;
     GameObject(GameObject&&) = delete;
     GameObject& operator=(GameObject&&) = delete;
+
+    /**
+     * @brief 添加组件 (里面会完成组件的 init())
+     * 
+     * @tparam T 组件类型
+     * @tparam Args 组件构造函数参数类型
+     * @param args 组件构造函数参数
+     * @return 组件指针
+     */
+    template<ComponentType T, typename... Args>
+    T* addComponent(Args&&... args)
+    {
+        // 获取类型标识。
+        /* typeid(T) -- 用于获取一个表达式或类型的运行时类型信息 (RTTI), 返回 std::type_info& */
+        /* std::type_index -- 针对std::type_info对象的包装器，主要设计用来作为关联容器（如 std::map）的键。*/
+        auto typeIndex = std::type_index{ typeid(T) };
+
+        // 如果组件已经存在，则直接返回组件指针
+        if (hasComponent<T>()) {
+            return getComponent<T>();
+        }
+
+        // 如果不存在则创建组件
+        /* std::forward -- 用于实现完美转发。传递多个参数的时候使用...标识 */
+        auto newComponent = std::make_unique<T>(std::forward<Args>(args)...);
+        T* ptr{ newComponent.get() }; // 先获取裸指针以便返回
+        newComponent->setOwner(this); // 设置组件的拥有者
+        /* newComponent 移动后变为空，不可再使用。因此必须使用 ptr 来调用组件的 init() 方法 */
+        m_components[typeIndex] = std::move(newComponent); // 移动组件到容器中
+        ptr->init();                                       // 初始化组件
+
+        spdlog::debug("GameObject::addComponent: added component {}", typeid(T).name());
+        return ptr; // 返回非拥有指针
+    }
 
     /**
      * @brief 获取组件

b39984af94c71cf3e416a117e855de8b24b11a44
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-04-14 16:23:51 +0800

    feat: Add 获取组件的方法

--- src/engine/object/game_object.h
+++ src/engine/object/game_object.h
@@ -29,6 +29,24 @@ public:
     GameObject(GameObject&&) = delete;
     GameObject& operator=(GameObject&&) = delete;
 
+    /**
+     * @brief 获取组件
+     * 
+     * @tparam T 组件类型
+     * @return 组件指针
+     */
+    template<ComponentType T>
+    T* getComponent() const
+    {
+        auto typeIndex = std::type_index{ typeid(T) };
+        auto it = m_components.find(typeIndex);
+        if (it != m_components.end()) {
+            // 返回unique_ptr的裸指针。
+            return static_cast<T*>(it->second.get());
+        }
+        return nullptr;
+    }
+
 private:
     std::unordered_map<std::type_index, std::unique_ptr<engine::component::ComponentBase>>
         m_components;             ///< @brief 组件列表

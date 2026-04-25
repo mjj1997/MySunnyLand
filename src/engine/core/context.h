ed2baa1ee762df1570e5d30c97b772f6bdfb5111
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-04-25 16:00:09 +0800

    feat(Context): Add 物理引擎模块到 Context 类中并更新构造函数和相关 getter

--- src/engine/core/context.h
+++ src/engine/core/context.h
@@ -13,6 +13,10 @@ namespace engine::resource {
 class ResourceManager;
 }
 
+namespace engine::physics {
+class PhysicsEngine;
+}
+
 namespace engine::core {
 
 /**
@@ -29,11 +33,13 @@ public:
      * @param renderer 对 Renderer 实例的引用。
      * @param camera 对 Camera 实例的引用。
      * @param resourceManager 对 ResourceManager 实例的引用。
+     * @param physicsEngine 对 PhysicsEngine 实例的引用。
      */
     Context(engine::input::InputManager& inputManager,
             engine::render::Renderer& renderer,
             engine::render::Camera& camera,
-            engine::resource::ResourceManager& resourceManager);
+            engine::resource::ResourceManager& resourceManager,
+            engine::physics::PhysicsEngine& physicsEngine);
 
     // 禁止拷贝和移动，Context 对象通常是唯一的或按需创建/传递
     Context(const Context&) = delete;
@@ -42,16 +48,14 @@ public:
     Context& operator=(Context&&) = delete;
 
     // --- Getters ---
-    engine::input::InputManager& inputManager() const ///< @brief 获取输入管理器
-    {
-        return m_inputManager;
-    }
+    ///< @brief 获取输入管理器
+    engine::input::InputManager& inputManager() const { return m_inputManager; }
     engine::render::Renderer& renderer() const { return m_renderer; } ///< @brief 获取渲染器
     engine::render::Camera& camera() const { return m_camera; }       ///< @brief 获取相机
-    engine::resource::ResourceManager& resourceManager() const        ///< @brief 获取资源管理器
-    {
-        return m_resourceManager;
-    }
+    ///< @brief 获取资源管理器
+    engine::resource::ResourceManager& resourceManager() const { return m_resourceManager; }
+    ///< @brief 获取物理引擎
+    engine::physics::PhysicsEngine& physicsEngine() const { return m_physicsEngine; }
 
 private:
     // 使用引用，确保每个模块都有效，使用时不需要检查指针是否为空。
@@ -59,6 +63,7 @@ private:
     engine::render::Renderer& m_renderer;                 ///< @brief 渲染器
     engine::render::Camera& m_camera;                     ///< @brief 相机
     engine::resource::ResourceManager& m_resourceManager; ///< @brief 资源管理器
+    engine::physics::PhysicsEngine& m_physicsEngine;      ///< @brief 物理引擎
 };
 
 } // namespace engine::core

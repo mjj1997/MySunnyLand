c36c73e20a4b854d6356b28d9d9e431391cc7c9f
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-04-29 17:03:15 +0800

    feat(PhysicsEngine): Add 存储本帧发生碰撞的 GameObject pair 的 vector 数据成员以及相应 getter

--- src/engine/physics/physics_engine.h
+++ src/engine/physics/physics_engine.h
@@ -8,6 +8,10 @@ namespace engine::component {
 class PhysicsComponent;
 }
 
+namespace engine::object {
+class GameObject;
+}
+
 namespace engine::physics {
 
 /**
@@ -31,6 +35,8 @@ public:
     void update(float deltaTime);
 
     // 设置器/获取器
+    ///< @brief 获取本帧发生的碰撞对 vector
+    const auto& collisionPairs() const { return m_collisionPairs; }
     ///< @brief 设置全局重力加速度
     void setGravity(const glm::vec2& gravity) { m_gravity = gravity; }
     const glm::vec2& gravity() const { return m_gravity; } ///< @brief 获取当前的全局重力加速度
@@ -40,6 +46,8 @@ public:
 private:
     ///< @brief 注册的物理组件容器，非拥有指针
     std::vector<engine::component::PhysicsComponent*> m_components;
+    ///< @brief 存储本帧发生的碰撞对
+    std::vector<std::pair<engine::object::GameObject*, engine::object::GameObject*>> m_collisionPairs;
 
     ///< @brief 默认重力值 (像素/秒^2, 相当于 100像素 对应现实 1m)
     glm::vec2 m_gravity{ 0.0f, 980.0f };

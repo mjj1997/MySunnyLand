c42103d73ca5348938f762e6c50ab8f7d76eaf11
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-05-17 17:00:09 +0800

    feat(PlayerComponent): Complete 组件的初始化逻辑

--- src/game/component/player_component.cpp
+++ src/game/component/player_component.cpp
@@ -1,5 +1,10 @@
 #include "player_component.h"
+#include "../../engine/component/physics_component.h"
+#include "../../engine/component/sprite_component.h"
+#include "../../engine/component/transform_component.h"
+#include "../../engine/object/game_object.h"
 #include "state/player_state_base.h"
+// #include "state/idle_state.h"
 
 #include <spdlog/spdlog.h>
 
@@ -23,6 +28,31 @@ void PlayerComponent::setState(std::unique_ptr<state::PlayerStateBase> newState)
 
 void PlayerComponent::init()
 {
+    if (!m_owner) {
+        spdlog::error("PlayerComponent 没有所属游戏对象!");
+        return;
+    }
+
+    // 获取必要的组件
+    m_transformComponent = m_owner->getComponent<engine::component::TransformComponent>();
+    m_physicsComponent = m_owner->getComponent<engine::component::PhysicsComponent>();
+    m_spriteComponent = m_owner->getComponent<engine::component::SpriteComponent>();
+
+    // 检查必要组件是否存在
+    if (!m_transformComponent || !m_physicsComponent || !m_spriteComponent) {
+        spdlog::error("Player 对象缺少必要组件！");
+        return;
+    }
+
+    // 初始化状态机
+    // m_currentState = std::make_unique<state::IdleState>(this);
+    if (m_currentState) {
+        setState(std::move(m_currentState));
+    } else {
+        spdlog::error("初始化玩家状态失败（make_unique 返回空指针）！");
+    }
+
+    spdlog::debug("PlayerComponent 初始化完成。");
 }
 
 void PlayerComponent::handleInput(engine::core::Context& context)

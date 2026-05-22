cf00f3a9cc9678ee96421d234d359bd5a63d9262
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-05-22 18:34:54 +0800

    feat(DeadState): Complete 进入死亡状态时的逻辑

--- src/game/component/state/dead_state.cpp
+++ src/game/component/state/dead_state.cpp
@@ -1,17 +1,42 @@
 #include "dead_state.h"
+#include "../player_component.h"
+
+#include "../../../engine/component/collider_component.h"
+#include "../../../engine/component/physics_component.h"
+#include "../../../engine/object/game_object.h"
+
+#include <spdlog/spdlog.h>
 
 namespace game::component::state {
 
-void DeadState::enter() {}
+void DeadState::enter()
+{
+    spdlog::debug("玩家进入死亡状态。");
+    // 播放死亡动画
+    playAnimation("dead");
+
+    // 造成向上击退效果
+    auto physicsComponent = m_playerComponent->physicsComponent();
+    physicsComponent->setVelocity(glm::vec2{ 0.0f, -200.0f });
+
+    // 禁用碰撞检测，形成自动掉出屏幕的效果
+    auto colliderComponent = m_playerComponent->owner()
+                                 ->getComponent<engine::component::ColliderComponent>();
+    if (colliderComponent) {
+        colliderComponent->setActive(false);
+    }
+}
 
 std::unique_ptr<PlayerStateBase> DeadState::handleInput(engine::core::Context& context)
 {
-    return std::unique_ptr<PlayerStateBase>();
+    // 死亡状态下不处理输入
+    return nullptr;
 }
 
 std::unique_ptr<PlayerStateBase> DeadState::update(float deltaTime, engine::core::Context& context)
 {
-    return std::unique_ptr<PlayerStateBase>();
+    // 死亡状态下不更新状态
+    return nullptr;
 }
 
 void DeadState::exit() {}

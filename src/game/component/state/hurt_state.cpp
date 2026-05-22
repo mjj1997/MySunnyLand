c433d26e8e9dec3aa97ed5d57f828a446a976890
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-05-22 17:05:41 +0800

    feat(HurtState): Complete 进入受伤状态时的逻辑

--- src/game/component/state/hurt_state.cpp
+++ src/game/component/state/hurt_state.cpp
@@ -1,13 +1,32 @@
 #include "hurt_state.h"
+#include "../player_component.h"
+#include "../../../engine/component/physics_component.h"
+#include "../../../engine/component/sprite_component.h"
+
+#include <glm/common.hpp>
 
 namespace game::component::state {
 
 void HurtState::enter()
 {
+    // 播放受伤动画
+    playAnimation("hurt");
+
+    // --- 造成击退效果 ---
+    auto physicsComponent = m_playerComponent->physicsComponent();
+    auto spriteComponent = m_playerComponent->spriteComponent();
+    auto knockbackVelocity = glm::vec2{ -100.0f, -150.0f }; // 默认往左上方击退
+    // 根据当前精灵的朝向状态，判断是否需要往右上方击退
+    if (spriteComponent->isFlipped()) {
+        knockbackVelocity.x = -knockbackVelocity.x; // 击退方向变为向右
+    }
+    // 设置击退速度
+    physicsComponent->setVelocity(knockbackVelocity);
 }
 
 std::unique_ptr<PlayerStateBase> HurtState::handleInput(engine::core::Context& context)
 {
+    // 受伤硬直期间，不能进行任何操控
     return nullptr;
 }
 

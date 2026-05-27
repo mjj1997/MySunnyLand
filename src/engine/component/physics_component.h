57fea6032ee46f8aa682dc1a8596fc8c62111cce
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-05-27 10:56:45 +0800

    feat(PhysicsComponent): Add 表示组件与梯子碰撞标志的数据成员以及相应 getter & setter

--- src/engine/component/physics_component.h
+++ src/engine/component/physics_component.h
@@ -71,6 +71,7 @@ public:
     bool isCollidedAbove() const { return m_isCollidedAbove; }   ///< @brief 检测组件是否与上方碰撞
     bool isCollidedLeft() const { return m_isCollidedLeft; }     ///< @brief 检测组件是否与左侧碰撞
     bool isCollidedRight() const { return m_isCollidedRight; }   ///< @brief 检测组件是否与右侧碰撞
+    bool isCollidedLadder() const { return m_isCollidedLadder; } ///< @brief 检测组件是否与梯子碰撞
 
     void setVelocity(const glm::vec2& velocity) { m_velocity = velocity; } ///< @brief 设置速度
     ///< @brief 设置质量，质量不能为负
@@ -86,6 +87,8 @@ public:
     void setCollidedLeft(bool collided) { m_isCollidedLeft = collided; }
     ///< @brief 设置组件与右侧碰撞标志
     void setCollidedRight(bool collided) { m_isCollidedRight = collided; }
+    ///< @brief 设置组件与梯子碰撞标志
+    void setCollidedLadder(bool collided) { m_isCollidedLadder = collided; }
 
 protected:
     // 核心循环方法
@@ -104,10 +107,11 @@ private:
     bool m_enabled{ true };             ///< @brief 组件是否激活
 
     // -- 碰撞状态标志 --
-    bool m_isCollidedBelow{ false }; ///< @brief 组件与下方碰撞标志
-    bool m_isCollidedAbove{ false }; ///< @brief 组件与上方碰撞标志
-    bool m_isCollidedLeft{ false };  ///< @brief 组件与左侧碰撞标志
-    bool m_isCollidedRight{ false }; ///< @brief 组件与右侧碰撞标志
+    bool m_isCollidedBelow{ false };  ///< @brief 组件与下方碰撞标志
+    bool m_isCollidedAbove{ false };  ///< @brief 组件与上方碰撞标志
+    bool m_isCollidedLeft{ false };   ///< @brief 组件与左侧碰撞标志
+    bool m_isCollidedRight{ false };  ///< @brief 组件与右侧碰撞标志
+    bool m_isCollidedLadder{ false }; ///< @brief 组件与梯子碰撞标志
 };
 
 } // namespace engine::component

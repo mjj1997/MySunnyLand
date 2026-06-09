3bd3cfc11c316d1f2743b0f3cfd6c039085cd056
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-06-09 16:25:20 +0800

    feat(UiManager): Complete 构造函数

--- src/engine/ui/ui_manager.cpp
+++ src/engine/ui/ui_manager.cpp
@@ -5,7 +5,12 @@
 
 namespace engine::ui {
 
-UiManager::UiManager() {}
+UiManager::UiManager()
+{
+    // 创建一个无特定大小和位置的Panel，它的子元素将基于它定位。
+    m_rootElement = std::make_unique<UiPanel>(glm::vec2{ 0.0f, 0.0f }, glm::vec2{ 0.0f, 0.0f });
+    spdlog::trace("UI 管理器构造完成。");
+}
 
 UiManager::~UiManager() = default;
 

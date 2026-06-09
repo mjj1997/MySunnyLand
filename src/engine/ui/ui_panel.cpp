d84a9f4d78840914ffcc19b369c53ecd3135e961
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-06-09 15:30:18 +0800

    feat(UiPanel): Add 表示背景色的数据成员和相应 getter & setter

--- src/engine/ui/ui_panel.cpp
+++ src/engine/ui/ui_panel.cpp
@@ -4,4 +4,14 @@ namespace engine::ui {
 
 void UiPanel::render(engine::core::Context& context) {}
 
+void UiPanel::setBackgroundColor(const std::optional<engine::utils::FColor>& backgroundColor)
+{
+    m_backgroundColor = backgroundColor;
+}
+
+const std::optional<engine::utils::FColor>& UiPanel::backgroundColor() const
+{
+    return m_backgroundColor;
+}
+
 } // namespace engine::ui
--- src/engine/ui/ui_panel.h
+++ src/engine/ui/ui_panel.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include "../utils/math.h"
 #include "ui_element_base.h"
+
+#include <optional>
 
 namespace engine::ui {
 
@@ -14,6 +17,12 @@ class UiPanel final : public UiElementBase
 {
 public:
     void render(engine::core::Context& context) override;
+
+    void setBackgroundColor(const std::optional<engine::utils::FColor>& backgroundColor);
+    const std::optional<engine::utils::FColor>& backgroundColor() const;
+
+private:
+    std::optional<engine::utils::FColor> m_backgroundColor; ///< @brief 可选背景色
 };
 
 } // namespace engine::ui

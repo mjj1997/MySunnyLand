a641f7d2f7badc467c27806fba50ff2bba688b75
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-06-10 16:10:25 +0800

    feat(UiLabel): Add 表示文本、字体的数据成员和相关 getters & setters

--- src/engine/ui/ui_label.cpp
+++ src/engine/ui/ui_label.cpp
@@ -1,7 +1,32 @@
 #include "ui_label.h"
+#include "../render/text_renderer.h"
 
 namespace engine::ui {
 
 void UiLabel::render(engine::core::Context& context) {}
+
+void UiLabel::setText(const std::string& text)
+{
+    m_text = text;
+    m_size = m_textRenderer.getTextSize(m_text, m_fontId, m_fontSize);
+}
+
+void UiLabel::setFontId(const std::string& fontId)
+{
+    m_fontId = fontId;
+    m_size = m_textRenderer.getTextSize(m_text, m_fontId, m_fontSize);
+}
+
+void UiLabel::setFontSize(int fontSize)
+{
+    m_fontSize = fontSize;
+    m_size = m_textRenderer.getTextSize(m_text, m_fontId, m_fontSize);
+}
+
+void UiLabel::setTextColor(const engine::utils::FColor& textColor)
+{
+    m_textColor = textColor;
+    /* 颜色变化, 不需要更新尺寸 */
+}
 
 } // namespace engine::ui
--- src/engine/ui/ui_label.h
+++ src/engine/ui/ui_label.h
@@ -2,6 +2,12 @@
 
 #include "ui_element_base.h"
 
+#include <string>
+
+namespace engine::render {
+class TextRenderer;
+}
+
 namespace engine::ui {
 
 /**
@@ -16,6 +22,28 @@ class UiLabel final : public UiElementBase
 {
 public:
     void render(engine::core::Context& context) override;
+
+    // --- Setters & Getters ---
+    const std::string& text() const { return m_text; }
+    const std::string& fontId() const { return m_fontId; }
+    int fontSize() const { return m_fontSize; }
+    const engine::utils::FColor& textColor() const { return m_textColor; }
+
+    void setText(const std::string& text);     ///< @brief 设置文本内容, 同时更新尺寸
+    void setFontId(const std::string& fontId); ///< @brief 设置字体ID, 同时更新尺寸
+    void setFontSize(int fontSize);            ///< @brief 设置字体大小, 同时更新尺寸
+    void setTextColor(const engine::utils::FColor& textColor);
+
+private:
+    ///< @brief 需要文本渲染器，用于获取和更新文本尺寸
+    engine::render::TextRenderer& m_textRenderer;
+
+    std::string m_text;   ///< @brief 文本内容
+    std::string m_fontId; ///< @brief 字体 ID
+    int m_fontSize;       ///< @brief 字体大小
+    engine::utils::FColor m_textColor{ 1.0f, 1.0f, 1.0f, 1.0f };
+
+    /* 可添加其他内容，例如边框、底色等 */
 };
 
 } // namespace engine::ui

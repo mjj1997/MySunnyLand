7857c815cd28d35a48ca0a4660eb959e99f3f4f0
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-06-10 10:56:59 +0800

    feat(UiImage): Complete 渲染逻辑

--- src/engine/ui/ui_image.cpp
+++ src/engine/ui/ui_image.cpp
@@ -1,4 +1,6 @@
 #include "ui_image.h"
+#include "../core/context.h"
+#include "../render/renderer.h"
 
 #include <spdlog/spdlog.h>
 
@@ -21,6 +23,20 @@ UiImage::UiImage(const std::string& textureId,
 
 void UiImage::render(engine::core::Context& context)
 {
+    if (!m_isVisible || m_sprite.textureId().empty()) {
+        return; // 如果不可见或纹理 ID 为空，不绘制
+    }
+
+    // 渲染自身
+    const auto& screenPos = screenPosition();
+    if (m_size.x == 0.0f && m_size.y == 0.0f) { // 如果尺寸为 0，则使用纹理的原始尺寸
+        context.renderer().drawUiSprite(m_sprite, screenPos);
+    } else { // 否则，使用指定的尺寸
+        context.renderer().drawUiSprite(m_sprite, screenPos, m_size);
+    }
+
+    // 渲染子元素（调用基类的 render 方法）
+    UiElementBase::render(context);
 }
 
 } // namespace engine::ui

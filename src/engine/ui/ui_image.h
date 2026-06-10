f6a67f42cb57ef812ee21dbcd82b365d565d2e65
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-06-10 10:55:18 +0800

    feat(UiImage): Add 表示精灵的数据成员以及相关的 getters & setters

--- src/engine/ui/ui_image.h
+++ src/engine/ui/ui_image.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "../render/sprite.h"
 #include "ui_element_base.h"
 
 namespace engine::ui {
@@ -13,6 +14,25 @@ class UiImage final : public UiElementBase
 {
 public:
     void render(engine::core::Context& context) override;
+
+    // --- Setters & Getters ---
+    const engine::render::Sprite& sprite() const { return m_sprite; }
+    void setSprite(const engine::render::Sprite& sprite) { m_sprite = sprite; }
+
+    const std::string& textureId() const { return m_sprite.textureId(); }
+    void setTextureId(const std::string& textureId) { m_sprite.setTextureId(textureId); }
+
+    const std::optional<SDL_FRect>& sourceRect() const { return m_sprite.sourceRect(); }
+    void setSourceRect(const std::optional<SDL_FRect>& sourceRect)
+    {
+        m_sprite.setSourceRect(sourceRect);
+    }
+
+    bool isFlipped() const { return m_sprite.isFlipped(); }
+    void setFlipped(bool flipped) { m_sprite.setFlipped(flipped); }
+
+private:
+    engine::render::Sprite m_sprite;
 };
 
 } // namespace engine::ui

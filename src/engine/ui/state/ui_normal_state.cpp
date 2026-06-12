2736b1dd5d887de7b5cb4b72a156dcdbd8256c78
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-06-12 16:00:36 +0800

    feat(UiNormalState): Derive UiNormalState from UiStateBase

---
+++ src/engine/ui/state/ui_normal_state.cpp
@@ -0,0 +1,13 @@
+#include "ui_normal_state.h"
+
+namespace engine::ui::state {
+
+void UiNormalState::enter()
+{
+}
+
+std::unique_ptr<UiStateBase> UiNormalState::handleInput(engine::core::Context& context)
+{
+}
+
+} // namespace engine::ui::state

747d7a6dcc9c7c6a3659c6b8c1561321d1b0cf83
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-06-12 16:23:47 +0800

    feat(UiHoverState): Derive UiHoverState form UiStateBase

---
+++ src/engine/ui/state/ui_hover_state.cpp
@@ -0,0 +1,29 @@
+#include "ui_hover_state.h"
+#include "../../core/context.h"
+#include "../../input/input_manager.h"
+#include "../ui_interactive_element_base.h"
+#include "ui_normal_state.h"
+
+namespace engine::ui::state {
+
+void UiHoverState::enter()
+{
+    // TODO: 设置 UI 为悬停状态的精灵
+}
+
+std::unique_ptr<UiStateBase> UiHoverState::handleInput(engine::core::Context& context)
+{
+    auto& inputManager = context.inputManager();
+    auto mousePos = inputManager.mousePosition();
+    if (!m_owner->isPointInside(mousePos)) { // 如果鼠标不在 UI 元素内，切换到正常状态
+        return std::make_unique<UiNormalState>(m_owner);
+    }
+
+    if (inputManager.isActionPressed("mouseLeftClick")) { // 如果鼠标点击了左键，切换到按下状态
+        // TODO: 切换到按下状态
+    }
+
+    return nullptr;
+}
+
+} // namespace engine::ui::state
---
+++ src/engine/ui/state/ui_hover_state.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "ui_state_base.h"
+
+namespace engine::ui::state {
+
+/**
+ * @brief 悬停状态
+ *
+ * 当鼠标悬停在可交互 UI 元素上时，会切换到该状态。
+ */
+class UiHoverState final : public UiStateBase
+{
+    friend class engine::ui::UiInteractiveElementBase;
+
+public:
+    UiHoverState(engine::ui::UiInteractiveElementBase* owner)
+        : UiStateBase{ owner }
+    {}
+
+    ~UiHoverState() override = default;
+
+private:
+    void enter() override;
+    std::unique_ptr<UiStateBase> handleInput(engine::core::Context& context) override;
+};
+
+} // namespace engine::ui::state

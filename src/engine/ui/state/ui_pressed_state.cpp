68e9f1b64cd27014a824a4e83ccb0d5c1ef9aa3f
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-06-12 16:33:12 +0800

    feat(UiPressedState): Derive UiPressedState from UiStateBase

---
+++ src/engine/ui/state/ui_pressed_state.cpp
@@ -0,0 +1,33 @@
+#include "ui_pressed_state.h"
+#include "../../core/context.h"
+#include "../../input/input_manager.h"
+#include "../ui_interactive_element_base.h"
+#include "ui_hover_state.h"
+#include "ui_normal_state.h"
+
+namespace engine::ui::state {
+
+void UiPressedState::enter()
+{
+    // TODO: 设置 UI 为按下状态的精灵
+    // TODO: 播放按下时的音效
+}
+
+std::unique_ptr<UiStateBase> UiPressedState::handleInput(engine::core::Context& context)
+{
+    auto& inputManager = context.inputManager();
+    auto mousePos = inputManager.mousePosition();
+    if (inputManager.isActionReleased("mouseLeftClick")) {
+        if (!m_owner->isPointInside(mousePos)) {
+            // 松开鼠标时，如果鼠标不在 UI 元素内，切换到正常状态
+            return std::make_unique<UiNormalState>(m_owner);
+        } else { // 松开鼠标时，如果鼠标在 UI 元素内，触发点击事件
+            // TODO: 触发点击事件
+            return std::make_unique<UiHoverState>(m_owner);
+        }
+    }
+
+    return nullptr;
+}
+
+} // namespace engine::ui::state
---
+++ src/engine/ui/state/ui_pressed_state.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "ui_state_base.h"
+
+namespace engine::ui::state {
+
+/**
+ * @brief 按下状态
+ *
+ * 当鼠标按下可交互 UI 元素时，会切换到该状态。
+ */
+class UiPressedState final : public UiStateBase
+{
+    friend class engine::ui::UiInteractiveElementBase;
+
+public:
+    UiPressedState(engine::ui::UiInteractiveElementBase* owner)
+        : UiStateBase{ owner }
+    {}
+
+    ~UiPressedState() override = default;
+
+private:
+    void enter() override;
+    std::unique_ptr<UiStateBase> handleInput(engine::core::Context& context) override;
+};
+
+} // namespace engine::ui::state

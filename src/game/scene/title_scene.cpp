5429bf864a350cd8bb3b1658169e6304e1f02293
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-06-14 11:00:09 +0800

    feat(TitleScene): Complete 带参数的构造函数

---
+++ src/game/scene/title_scene.cpp
@@ -0,0 +1,22 @@
+#include "title_scene.h"
+#include "../data/session_data.h"
+
+#include <spdlog/spdlog.h>
+
+namespace game::scene {
+
+TitleScene::TitleScene(engine::core::Context& context,
+                       engine::scene::SceneManager& sceneManager,
+                       std::shared_ptr<game::data::SessionData> gameSessionData)
+    : SceneBase{ "TitleScene", context, sceneManager }
+    , m_gameSessionData{ std::move(gameSessionData) }
+{
+    if (m_gameSessionData == nullptr) {
+        // 如果没有传入 SessionData，创建一个默认的
+        m_gameSessionData = std::make_shared<game::data::SessionData>();
+        spdlog::info("未提供 SessionData，使用默认值。");
+    }
+    spdlog::trace("TitleScene 构造完成。");
+}
+
+} // namespace game::scene

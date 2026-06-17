db2848883edb0c01483a46940913ebd89935e054
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-06-17 16:47:50 +0800

    feat: Add 设置初始场景的函数

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include "engine/core/game_app.h"
+#include "engine/scene/scene_manager.h"
+#include "game/scene/title_scene.h"
 
 #include <spdlog/spdlog.h>
+
+void setupInitialScene(engine::scene::SceneManager& sceneManager)
+{
+    // GameApp在调用run方法之前，先创建并设置初始场景
+    auto initialScene = std::make_unique<game::scene::TitleScene>(sceneManager.context(),
+                                                                  sceneManager);
+    sceneManager.requestPushScene(std::move(initialScene));
+}
 
 int main(int, char*[])
 {
